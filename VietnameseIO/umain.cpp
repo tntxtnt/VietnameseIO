4843835b4662e217c7b8c4dcffada3c699755087
Tri Tran <lepnit205@gmail.com>
2015-06-02 23:37:02 -0500

    nhập tên, tuổi, giới tính

--- VietnameseIO/umain.cpp
+++ VietnameseIO/umain.cpp
@@ -7,21 +7,33 @@ int main()
 {
     _setmode(_fileno(stdout), _O_U16TEXT); //needed for output
     _setmode(_fileno(stdin), _O_U16TEXT);  //needed for input
+    // nhớ chuyển font của console sang Consolas (size 16)
 
-    std::wcout << L"Chương trình đọc và xuất chuỗi tiếng Việt\n\n";
+    std::wcout << L"Chương trình đọc và xuất chuỗi tiếng Việt\n\n"
+               << L"  \"Gõ\" tiếng Việt bằng cách copy chuỗi tiếng Việt ở trình soạn thảo tiếng Việt\n"
+               << L"nào đó rồi click phải chuột vào màn hình console\n\n";
 
+    // nhập họ tên
     std::wstring username;
     std::wcout << L"Nhập họ tên của bạn: ";
     std::getline(std::wcin, username);
-    
-    std::wcout << L"Xin chào, " << username << L"!\n";
+    std::wcout << L">> Xin chào, " << username << L"!\n\n";
 
-    int gender;
-    std::wcout << L"Nhập 1 nếu bạn là nam, nhập 2 nếu bạn là nữ: ";
+    // nhập giới tính
+    wchar_t gender;
+    std::wcout << L"Bạn là nam hay nữ:\n a. Nam\n b. Nữ\n";
     std::wcin >> gender;
+    std::wcout << L">> Giới tính: ";
+    if      (gender == L'a') std::wcout << L"Nam\n\n";
+    else if (gender == L'b') std::wcout << L"Nữ\n\n";
+    else                     std::wcout << L"Không xác định\n\n";
 
-    std::wcout << L"Giới tính: ";
-    if (gender == 1)      std::wcout << L"Nam\n";
-    else if (gender == 2) std::wcout << L"Nữ\n";
-    else                  std::wcout << L"Không xác định\n";
+    // nhập tuổi
+    int age;
+    std::wcout << L"Nhập tuổi của bạn: ";
+    std::wcin >> age;
+    std::wcout << L">> Năm nay bạn được " << age << L" tuổi\n\n";
+    
+
+    std::wcout << L"Chương trình kết thúc.\n";
 }
